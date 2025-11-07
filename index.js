e901d0e0a8a4133f986738f13b65998758ea7cbd
inclan54321 <tuemail@ejemplo.com>
2025-11-06 21:23:36 -0600

    agregar diseño con imagen de fondo

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, '0.0.0.0', () => {
-  console.log(Servidor funcionando en puerto ${PORT});
+  console.log(`Servidor funcionando en puerto ${PORT}`);
 });
